001469c2c32296d9d2294545f1bb89a9814e77e7
IteratorW <rob.bog@list.ru>
2019-07-12 19:34:08 +0500

    ынишал

---
+++ src/main/java/ru/allformine/bans/afmbans/AFMBans.java
@@ -0,0 +1,28 @@
+package ru.allformine.bans.afmbans;
+
+import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(
+        id = "afmbans",
+        name = "AFMBans",
+        description = "Custom ban plugin.",
+        url = "allformine.ru",
+        authors = {
+                "Iterator",
+                "HeroBrine1st_Erq"
+        }
+)
+public class AFMBans {
+
+    @Inject
+    private Logger logger;
+
+    @Listener
+    public void onServerStart(GameStartedServerEvent event) {
+
+    }
+}
