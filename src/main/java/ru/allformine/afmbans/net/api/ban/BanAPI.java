3a267d58c47eb4e49228f56ce560478c229a87b7
HeroBrine1st <herobrine1.videos@gmail.com>
2019-07-14 17:23:40 +0300

    Готово для тестов нахуй

--- src/main/java/ru/allformine/afmbans/net/api/ban/BanAPI.java
+++ src/main/java/ru/allformine/afmbans/net/api/ban/BanAPI.java
@@ -1,23 +1,21 @@
 package ru.allformine.afmbans.net.api.ban;
 
 import com.google.gson.JsonObject;
+import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.entity.living.player.Player;
 import ru.allformine.afmbans.net.JsonRequest;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.UUID;
 
 public class BanAPI {
     private UUID uuid;
-
+    private String nickname;
     public BanAPI(Player player){
         this.uuid = player.getUniqueId();
+        this.nickname = player.getName();
     }
 
-    public BanAPI(UUID uuid){
-        this.uuid = uuid;
-    }
 
     private static JsonObject makeRequest(String method, JsonObject json) throws Exception {
         JsonRequest req = new JsonRequest(
@@ -37,7 +35,7 @@ public class BanAPI {
 
     public boolean check(Type type) throws Exception {
         JsonObject json = new JsonObject();
-        json.addProperty("uuid", this.uuid.toString());
+        json.addProperty("UUID", this.uuid.toString());
         json.addProperty("type", type.name());
         JsonObject res = makeRequest("check", json);
         return res.get("punished").getAsBoolean();
@@ -45,12 +43,20 @@ public class BanAPI {
 
     public int getWarns() throws Exception {
         JsonObject json = new JsonObject();
-        json.addProperty("uuid", this.uuid.toString());
+        json.addProperty("UUID", this.uuid.toString());
         json.addProperty("type", "Warn");
         JsonObject res = makeRequest("check", json);
         return res.get("count").getAsInt();
     }
 
-
+    public int warn(String reason, CommandSource source) throws Exception {
+        JsonObject json = new JsonObject();
+        json.addProperty("target_UUID", this.uuid.toString());
+        json.addProperty();
+        json.addProperty("type", "warn");
+        json.addProperty("reason", reason);
+        JsonObject res = makeRequest("punish", json);
+        return res.get("count").getAsInt();
+    }
 
 }
