316a64bab34db7e3960d6839a16fbbc07b17a340
Rob <rob.bog@list.ru>
2019-07-29 00:52:02 +0300

    локалхост

--- src/main/java/ru/allformine/afmbans/net/api/ban/BanAPI.java
+++ src/main/java/ru/allformine/afmbans/net/api/ban/BanAPI.java
@@ -20,7 +20,7 @@ public class BanAPI {
     private static JsonObject makeRequest(String method, JsonObject json) throws Exception {
         JsonRequest req = new JsonRequest(
                 new URL(
-                        String.format("http://allformine/ban_api?method=%s", method)
+                        String.format("http://127.0.0.1/ban_api?method=%s", method)
                 ),
                 json
         );
