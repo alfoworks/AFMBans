511c963d9857cedd50697f49158609c353b99526
Rob <rob.bog@list.ru>
2019-07-30 01:52:24 +0300

    Ебал спонж в ачко

--- src/main/java/ru/allformine/afmbans/PluginUtils.java
+++ src/main/java/ru/allformine/afmbans/PluginUtils.java
@@ -28,9 +28,9 @@ public class PluginUtils {
         Text text = Text.builder()
                 .append(Text.of(src.getName())).color(PluginStatics.BROADCAST_COLOR)
                 .append(Text.of(action))
-                .append(Text.of(" игрока")).color(TextColors.RESET)
+                .append(Text.builder().append(Text.of(" игрока")).color(TextColors.RESET).build())
                 .append(Text.of(target)).color(PluginStatics.BROADCAST_COLOR)
-                .append(Text.of("навсегда.")).color(TextColors.RESET)
+                .append(Text.builder().append(Text.of(" навсегда")).color(TextColors.RESET).build())
                 .build();
 
         Sponge.getServer().getBroadcastChannel().send(text);
@@ -40,10 +40,10 @@ public class PluginUtils {
         Text message = Text.builder()
                 .append(Text.of("Вам перманентный бан :3")).color(PluginStatics.BAN_MESSAGE_COLOR)
                 .append(Text.of("\n\n"))
-                .append(Text.of("От: ")).color(TextColors.RESET)
-                .append(Text.of(source)).color(PluginStatics.BAN_MESSAGE_COLOR)
-                .append(Text.of("\n\n"))
-                .append(Text.of("По причине: ")).color(TextColors.RESET)
+                .append(Text.builder().append(Text.of("От: ")).color(TextColors.RESET).build())
+                .append(Text.of(source))
+                .append(Text.of("\n"))
+                .append(Text.builder().append(Text.of("По причине: ")).color(TextColors.RESET).build())
                 .append(Text.of(reason)).color(PluginStatics.BAN_MESSAGE_COLOR)
                 .build();
 
