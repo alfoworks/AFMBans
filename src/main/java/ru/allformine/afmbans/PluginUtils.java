365c6614372d1cc85b6ccdfaaf9f7e420d9af993
Rob <rob.bog@list.ru>
2019-07-30 02:48:42 +0300

    Real jija

--- src/main/java/ru/allformine/afmbans/PluginUtils.java
+++ src/main/java/ru/allformine/afmbans/PluginUtils.java
@@ -37,7 +37,7 @@ public class PluginUtils {
         Text text = Text.builder()
                 .append(Text.of(src.getName() + " ")).color(PluginStatics.BROADCAST_COLOR)
                 .append(Text.of(action + " "))
-                .append(Text.builder().append(Text.of(" игрока ")).color(TextColors.RESET).build())
+                .append(Text.builder().append(Text.of("игрока ")).color(TextColors.RESET).build())
                 .append(Text.of(target)).color(PluginStatics.BROADCAST_COLOR)
                 .append(type != ActionType.UNBAN && type != ActionType.UNMUTE && type != ActionType.UNWARN ? Text.builder().append(Text.of(" навсегда ")).color(TextColors.RESET).build() : null)
                 .build();
@@ -48,13 +48,13 @@ public class PluginUtils {
     public static Text getBanMessageForPlayer(String source, String reason) {
         return Text.builder()
                 .append(Text.of("Вам"))
-                .append(Text.builder().append(Text.of("перманентный")).color(PluginStatics.BAN_MESSAGE_COLOR).build())
+                .append(Text.builder().append(Text.of(" перманентный ")).color(PluginStatics.BAN_MESSAGE_COLOR).build())
                 .append(Text.of("бан :3"))
-                .append(Text.of("\n\n")).color(PluginStatics.BAN_MESSAGE_COLOR)
-                .append(Text.builder().append(Text.of("От: ")).color(TextColors.RESET).build())
+                .append(Text.of("\n\n"))
+                .append(Text.builder().append(Text.of("От: ")).color(PluginStatics.BAN_MESSAGE_COLOR).build())
                 .append(Text.of(source))
                 .append(Text.of("\n"))
-                .append(Text.builder().append(Text.of("По причине: ")).color(TextColors.RESET).build())
+                .append(Text.builder().append(Text.of("По причине: ")).color(PluginStatics.BAN_MESSAGE_COLOR).build())
                 .append(Text.of(reason)).color(PluginStatics.BAN_MESSAGE_COLOR)
                 .build();
     }
