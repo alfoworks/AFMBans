91d812aced905460e9d802e9c61e78c59e07ed0b
HeroBrine1st_Erquilenne <123@mail.ru>
2019-08-05 22:13:47 +0300

    Идея, замолчи блядь

--- src/main/java/ru/allformine/afmbans/net/api/ban/response/CheckResponse.java
+++ src/main/java/ru/allformine/afmbans/net/api/ban/response/CheckResponse.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@SuppressWarnings("WeakerAccess")
 public class CheckResponse {
     public boolean ok;
     public boolean punished;
