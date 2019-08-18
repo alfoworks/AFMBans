7a4a2caf22320a205eb10e3beef8982a67f165be
HeroBrine1st_Erquilenne <123@mail.ru>
2019-08-18 19:32:16 +0300

    Идея, заебала, это не мой код, на жри фикс нахуй

--- src/main/java/ru/allformine/afmbans/commands/CommandCheckPlayer.java
+++ src/main/java/ru/allformine/afmbans/commands/CommandCheckPlayer.java
@@ -5,28 +5,40 @@ import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.text.Text;
 import ru.allformine.afmbans.PluginUtils;
 import ru.allformine.afmbans.net.api.ban.BanAPI;
 import ru.allformine.afmbans.net.api.ban.PunishType;
 import ru.allformine.afmbans.net.api.ban.response.CheckResponse;
+import ru.allformine.afmbans.net.api.ban.response.IpHistoryResponse;
+import ru.allformine.afmbans.net.api.ban.response.object.IpHistoryRecord;
+
+import java.util.List;
+import java.util.Optional;
 
 @Deprecated
 public class CommandCheckPlayer extends Command {
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-        String nick = args.<String>getOne("player").get();
-        BanAPI banApi = new BanAPI(nick);
+        Optional<String> protoNick = args.<String>getOne("player");
+        if(protoNick.isPresent()){
+            String nick = protoNick.get();
+            BanAPI banApi = new BanAPI(nick);
 
-        CheckResponse resp;
+            CheckResponse resp;
 
-        try {
-            resp = banApi.check(PunishType.BAN, Sponge.getServer().getPlayer(nick).isPresent() ? Sponge.getServer().getPlayer(nick).get().getConnection().getAddress().getAddress() : null);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new CommandException(getReplyText("Произошла неизвестная ошибка.", TextType.ERROR));
+            try {
+                resp = banApi.check(PunishType.BAN, Sponge.getServer().getPlayer(nick).isPresent() ? Sponge.getServer().getPlayer(nick).get().getConnection().getAddress().getAddress() : null);
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new CommandException(getReplyText("Произошла неизвестная ошибка.", TextType.ERROR));
+            }
+
+            src.sendMessage(getReplyText(String.format(resp.target + " забанне: %s", resp.punished ? ("да. " + PluginUtils.getOneLineBanMessageForPlayer(resp)) : "нет."), TextType.OK));
+
+        }else{
+            src.sendMessage(getReplyText("Недостаточно аргументов!", TextType.ERROR));
         }
-
-        src.sendMessage(getReplyText(String.format(resp.target + " забанне: %s", resp.punished ? ("да. " + PluginUtils.getOneLineBanMessageForPlayer(resp)) : "нет."), TextType.OK));
 
         return CommandResult.success();
     }
