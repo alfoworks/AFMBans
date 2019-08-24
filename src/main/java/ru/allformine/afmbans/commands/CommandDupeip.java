79374ba7f014c12a8b4dddb4a12aa779d7ffe20c
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2019-08-24 20:41:31 +0300

    Dupeip готов?

--- src/main/java/ru/allformine/afmbans/commands/CommandDupeip.java
+++ src/main/java/ru/allformine/afmbans/commands/CommandDupeip.java
@@ -1,13 +1,14 @@
 package ru.allformine.afmbans.commands;
 
+import com.google.gson.Gson;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.format.TextColor;
 import org.spongepowered.api.text.format.TextColors;
 import ru.allformine.afmbans.PluginMessages;
+import ru.allformine.afmbans.PluginUtils;
 import ru.allformine.afmbans.net.api.ban.BanAPI;
 import ru.allformine.afmbans.net.api.ban.error.ApiError;
 import ru.allformine.afmbans.net.api.ban.response.IpHistoryResponse;
@@ -33,33 +34,24 @@ public class CommandDupeip extends Command {
         if(protoNick.isPresent()){
             String nick = protoNick.get();
             IpHistoryResponse ipHistoryResponse;
+            Map<String, Boolean> response;
             try {
                 InetAddress address = getIpFromHistory(nick).ip;
                 ipHistoryResponse = BanAPI.getIpHistory(null, address);
-            } catch (Exception e) { // ApiError, IOE
-                e.printStackTrace();
-                throw new CommandException(getReplyText(PluginMessages.UNKNOWN_ERROR, TextType.ERROR));
-            }
-            List<IpHistoryRecord> items = ipHistoryResponse.items;
-            List<String> nicknames = new ArrayList<>();
-            for(IpHistoryRecord record: items) nicknames.add(record.nickname);
-            Map<String, Boolean> response;
-            try {
+                List<IpHistoryRecord> items = ipHistoryResponse.items;
+                List<String> nicknames = new ArrayList<>();
+                for(IpHistoryRecord record: items) nicknames.add(record.nickname);
                 response = BanAPI.massBanCheck(nicknames);
-            } catch (Exception e) { // ApiError, IOE
+            } catch (ApiError | IOException e) {
                 e.printStackTrace();
                 throw new CommandException(getReplyText(PluginMessages.UNKNOWN_ERROR, TextType.ERROR));
             }
             Text.Builder builder = Text.builder();
             builder.append(Text.of("Известные аккаунты игрока " + nick + ":\n"));
-            response.forEach((nickname, punished) -> {
-                TextColor color = TextColors.GREEN;
-                if(punished){
-                    color = TextColors.RED;
-                }
-                builder.append(colorText(nickname, color));
-                builder.append(Text.of(", "));
-            });
+            PluginUtils.debug(new Gson().toJson(response));
+            List<Text> text = new ArrayList<>();
+            response.forEach((nickname, punished) -> text.add(colorText(nickname, punished ? TextColors.RED : TextColors.GREEN)));
+            builder.append(Text.joinWith(Text.of(", "), text));
             src.sendMessage(builder.build());
         }else{
             src.sendMessage(getReplyText("Недостаточно аргументов!", TextType.ERROR));
